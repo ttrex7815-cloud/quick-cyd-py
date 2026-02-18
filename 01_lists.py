1a62b6746b7fea490ce81b186730ed3944d22ffb
ttrex7815-cloud <ttrex7815@gmail.com>
2026-02-18 21:17:24 +0000

    Added lists review, turned off AI companion by default

---
+++ 01_lists.py
@@ -0,0 +1,41 @@
+# Part 1 - the seven (or eight) dwarves
+#
+# a. Replace "None" on line 2 with a list that contains the names of the seven dwarves
+# Grumpy, Happy, Sleepy, Sneezy, Dopey, Bashful, and Doc
+# dwarves = None
+# print("The seven dwarves: " + str(dwarves))
+
+# b. Join the crew! Write one line below that adds your name 
+#    write a line that prints the new list
+
+# c. There are two ways to print out your name from the list using indexes. Do them both
+# print(dwarves[8])
+# print(dwarves[8])
+
+# d. Uncomment the line below and print Happy's name using his index in the list
+# print(dwarves[8])
+
+# e. Maybe eight is two many, write a line to remove a specific dwarf's name
+# (this can be you or another dwarf)
+
+# f. Write one line of code below so that when the list is printed again it is
+#    in alphabetical order
+
+
+# print("Seven alphabetical dwarves: " + str(dwarves))
+
+# Part 2 - counting
+#
+# a. create a variable named one_to_ten that contains the numbers 0 to 100
+#    use the range function to do it without typing all the number yourself
+
+# b. use the range function a variable named evens that contains even numbers
+#    from 0 to 100 
+#    hint: the range function's third parameter will help with this
+#    https://www.w3schools.com/python/ref_func_range.asp
+
+# c. use range to create a list that counts *backward* from one to 100
+
+
+# d. use range to create a list that counts *backward by 5* fromn 100
+#    as in 100, 95, 90, 85, 80...
